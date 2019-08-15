46ee1f7e4d5550212f7d939ed8bc74a7b2b168de
Robert Ovens <robert.ovens@globalx.com.au>
2019-08-16 09:28:54 +1000

    get data method

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace name_sorter2
+{
+    class Program
+    {
+        static IEnumerable<string> GetData(string fileName)
+        {
+            var lines = File.ReadLines(fileName);
+            return lines;
+            
+        }
+        
+        static void Main(string[] args)
+        {
+
+            var listData = GetData("unsorted-names-list.txt");
+            listData.ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+
+        
+
+//        static string SortData(string ListData)
+//        {
+//            
+//        }
+    }
+    
+    
+}
