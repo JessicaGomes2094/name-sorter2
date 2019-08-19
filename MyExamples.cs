841899141a2b02032abc6d7ee8581906862230c8
Robert Ovens <robert.ovens@globalx.com.au>
2019-08-20 09:17:36 +1000

    class methods

---
+++ MyExamples.cs
@@ -0,0 +1,21 @@
+namespace name_sorter2
+{
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+            
+        }
+        
+        // Multiply 
+        // Divide
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -5,28 +5,40 @@ using System.Linq;
 
 namespace name_sorter2 {
     class Program {
-        static IEnumerable<string> GetData (string fileName) {
-            var lines = File.ReadLines (fileName);
-            return lines;
+//        static IEnumerable<string> GetData (string fileName) {
+//            var lines = File.ReadLines (fileName);
+//            return lines;
+//
+//        }
+//        static string SortData (string ListData) {
+//            var lines = File.ReadLines (ListData);
+//                //ListData.SortData();
+//                //ListData.OrderBy (item => item.Split('.').Last());
+//                //ListData.OrderBy OrderBy(x => x.LastName).ThenBy(x => x.MiddleName).ThenBy(x => x.FirstName);
+//            return ListData;
+//        }
 
-        }
-        static string SortData (string ListData) {
-            var lines = File.ReadLines (ListData);
-                //ListData.SortData();
-                //ListData.OrderBy (item => item.Split('.').Last());
-                //ListData.OrderBy OrderBy(x => x.LastName).ThenBy(x => x.MiddleName).ThenBy(x => x.FirstName);
-            return ListData;
-        }
+        static void Main (string[] args)
+        {
 
-        static void Main (string[] args) {
-
-            var listData = GetData ("unsorted-names-list.txt");
-            listData.ToList ().ForEach (x => Console.WriteLine (x));
-
-            //  var SortValue = SortData ("Sorted Names");
-            //  SortValue.ToList ().ForEach (x => Console.WriteLine (x));
+            var a = 2;
+            var b = 3;
+      
+            var calc = new Calc();
+            var myanswer = calc.Add(a, b);
+            Console.WriteLine(myanswer);
+            var answer = calc.Subtract(a, b);
+            Console.WriteLine(answer);
+//Examples of Multiply and Divide  
+            
+//            var listData = GetData ("unsorted-names-list.txt");
+//            listData.ToList ().ForEach (x => Console.WriteLine (x));
+//    
+//              var sortedNames = SortData ("Sorted Names");
+//              
+//           //   SortValue.ToList ().ForEach (x => Console.WriteLine (x));
 
         }
     }
-
 }
+
